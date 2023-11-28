5c42152d41ffba36c30f103f9d0cd0f1fdebc89b
anotherITdude <abhi.baidya@gmail.com>
2023-11-28 20:57:54 +0100

    Fix: text change

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="border-b">
       <div className="flex h-16 justify-between items-center px-4">
         <div className="text-sm md:text-md">
-          Alain Back to School Campaign August 2023
+          Alain Staycation Campaign November 2023
         </div>
         <div className="bg-red-500 rounded-full p-[2px]">
           <UserButton afterSignOutUrl="/" />
